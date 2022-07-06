5a8fb176fc5506a2d25a62a4661f115d2f505774
Roelvis Coello <rcabijana@gmail.com>
2022-07-06 18:02:19 -0400

    Add project files.

---
+++ preOrder.cpp
@@ -0,0 +1,96 @@
+// preOrder.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+class Solution {
+public:
+    Node* insert(Node* root, int data) {
+        if (root == NULL) {
+            return new Node(data);
+        }
+        else {
+            Node* cur;
+            if (data <= root->data) {
+                cur = insert(root->left, data);
+                root->left = cur;
+            }
+            else {
+                cur = insert(root->right, data);
+                root->right = cur;
+            }
+
+            return root;
+        }
+    }
+
+    /* you only have to complete the function given below.
+    Node is defined as
+
+    class Node {
+        public:
+            int data;
+            Node *left;
+            Node *right;
+            Node(int d) {
+                data = d;
+                left = NULL;
+                right = NULL;
+            }
+    };
+
+    */
+
+    void preOrder(Node* root) {
+        if (root == NULL)
+            return;
+
+        cout << root->data + " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+
+};
+int main()
+{
+    Solution myTree;
+    Node* root = NULL;
+
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while (t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+
+    myTree.preOrder(root);
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
